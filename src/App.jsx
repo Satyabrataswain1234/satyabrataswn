6633f05fb39c4eb79cdd645e5f24cdd4dbb34bbe
Satyabrataswain1234 <147232621+Satyabrataswain1234@users.noreply.github.com>
2024-09-24 13:09:12 +0530

    1st commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import DataTable from "./assets/components/DataTable";
+
+function App() {
+  const data = [
+    { id: 1, name: "Satya", age: 28 },
+    { id: 2, name: "Sunil", age: 32 },
+    { id: 3, name: "Rinku", age: 25 },
+    { id: 4, name: "Pabitra", age: 29 },
+    { id: 5, name: "Anurag", age: 35 },
+    { id: 6, name: "Saikat", age: 20 }
+  ];
+
+  return (
+    <div className="App">
+      <h1>Data Table</h1>
+      <DataTable data={data} />
+    </div>
+  );
+}
+
+export default App;
